2e4d2aea378a22afa7601be84eb96607a4a393f5
TBoileau <t-boileau@email.com>
2022-01-06 18:28:04 +0100

    feat: implement User entity

---
+++ src/Repository/UserRepository.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Repository;
+
+use App\Entity\User;
+use App\Gateway\UserGateway;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @template-extends ServiceEntityRepository<User>
+ */
+final class UserRepository extends ServiceEntityRepository implements UserGateway
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, User::class);
+    }
+}
